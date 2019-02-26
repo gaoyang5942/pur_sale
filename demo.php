4aa0d4c1cbc83cba1f07f81139f01eb744e749cd
gaoyang <gaoyang.jixiang.cn>
2019-02-26 09:29:27 +0800

    测试多个文件

--- demo.php
+++ demo.php
@@ -1,11 +1,12 @@
 <?php
 class Demo{
 	private $de = 11;
+	protected $demo = 22;
 	public function aaa(){
 		$bb = 105*1000;
 		$cc = 3.53*300;
 		$dd = $this->de;
-		return ($bb+$cc);
+		return ($bb+$cc+$this->$demo);
 	}
 	public function bbb(){
 		return $this->aaa();
