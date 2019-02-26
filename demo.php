b38ce4adb6db9a33ee9e7805b7257840fe199f01
gaoyang <gaoyang.jixiang.cn>
2019-02-26 09:20:24 +0800

    测试

---
+++ demo.php
@@ -0,0 +1,17 @@
+<?php
+class Demo{
+	private $de = 11;
+	public function aaa(){
+		$bb = 15*1000;
+		 $cc = 3.53*300;
+		$dd = $this->de;
+		return ($bb+$cc);
+	}
+	public function bbb(){
+		return $this->aaa();
+	}
+}
+	$ba = new demo;
+	echo $ba->bbb();
+?>
+
